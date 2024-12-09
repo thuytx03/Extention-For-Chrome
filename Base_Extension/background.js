eacf25700b08b2596a685f40fda238fbe1c1feac
thuytx03 <111112338+thuytx03@users.noreply.github.com>
2024-12-09 22:39:38 +0700

    update docs

--- Base_Extension/background.js
+++ Base_Extension/background.js
@@ -1,1 +1,9 @@
-console.log("background.js");
+// background.js
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed!");
+  });
+  
+  chrome.action.onClicked.addListener(() => {
+    console.log("Extension icon clicked!");
+  });
+  
